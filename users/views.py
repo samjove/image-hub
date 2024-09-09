78a78602df4cb23f0ef363c40f2926c01cdbf185
samjove <vermo7en@gmail.com>
2024-09-09 15:06:56 -0400

    Added registration endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
